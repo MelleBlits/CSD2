6b2543866e33e8aadfd974578a1506a8444a75d8
Melle Blits <melle.blits@student.hku.nl>
2021-09-14 22:27:21 +0200

    Playing a Sample a User specified number of Times

--- CSD2A/pythonBasics/playsound.py
+++ CSD2A/pythonBasics/playsound.py
@@ -1,3 +1,14 @@
-import simpleaudio.functionchecks as fc
+# import simpleaudio.functionchecks as fc
 
-fc.LeftRightCheck.run()
+# fc.LeftRightCheck.run()
+# van Joris Gekregen 
+import simpleaudio as sa
+filename ='SNARE1.wav'
+wave_obj = sa.WaveObject.from_wave_file(filename)
+
+print('How many Times would you like to Play the Sample')
+playTimes = int(input())
+
+for x in range(playTimes):
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
